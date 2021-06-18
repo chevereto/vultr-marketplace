20eeae1c04799fef4292bd35d0fa84474ab46f3a
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-06-17 22:21:52 -0400

    fp

--- init.sh
+++ init.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-source chevereto
+source ./chevereto
 
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[. / 0]}")" &>/dev/null && pwd)"
 WORKING_DIR="/var/www/html"
 CHEVERETO_SOFTWARE=${SOFTWARE_CHEVERETO}
 CHEVERETO_TAG=${SOFTWARE_TAG}
 CHEVERETO_INSTALLER_TAG=${SOFTWARE_INSTALLER_TAG}
 CHEVERETO_LICENSE=
 
-source scripts/00-update.sh
-source scripts/01-fs.sh
-source scripts/10-php.sh
-source scripts/11-installer.sh
-source scripts/12-apache.sh
-source common/scripts/03-force-ssh-logout.sh
-source common/scripts/90-cleanup.sh
-source scripts/99-apache-reload.sh
+source ./scripts/00-update.sh
+source ./scripts/01-fs.sh
+source ./scripts/10-php.sh
+source ./scripts/11-installer.sh
+source ./scripts/12-apache.sh
+source ./common/scripts/03-force-ssh-logout.sh
+source ./common/scripts/90-cleanup.sh
+source ./scripts/99-apache-reload.sh
 
 echo "[OK] Installer $SOFTWARE_INSTALLER_TAG provisioned"
