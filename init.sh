500c75010bd36ad4223e15181bbcc8b7dd180f72
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-06-17 23:02:48 -0400

    wip

--- init.sh
+++ init.sh
@@ -2,22 +2,21 @@
 
 set -e
 
-. ./chevereto
-
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[. / 0]}")" &>/dev/null && pwd)"
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 WORKING_DIR="/var/www/html"
+. "$PROJECT_DIR/chevereto"
 CHEVERETO_SOFTWARE=${SOFTWARE_CHEVERETO}
 CHEVERETO_TAG=${SOFTWARE_TAG}
 CHEVERETO_INSTALLER_TAG=${SOFTWARE_INSTALLER_TAG}
 CHEVERETO_LICENSE=
 
-. ./scripts/00-update.sh
-. ./scripts/01-fs.sh
-. ./scripts/10-php.sh
-. ./scripts/11-installer.sh
-. ./scripts/12-apache.sh
-. ./common/scripts/03-force-ssh-logout.sh
-. ./common/scripts/90-cleanup.sh
-. ./scripts/99-apache-reload.sh
+. "$PROJECT_DIR/scripts/00-update.sh"
+. "$PROJECT_DIR/scripts/01-fs.sh"
+. "$PROJECT_DIR/scripts/10-php.sh"
+. "$PROJECT_DIR/scripts/11-installer.sh"
+. "$PROJECT_DIR/scripts/12-apache.sh"
+. "$PROJECT_DIR/common/scripts/03-force-ssh-logout.sh"
+. "$PROJECT_DIR/common/scripts/90-cleanup.sh"
+. "$PROJECT_DIR/scripts/99-apache-reload.sh"
 
 echo "[OK] Installer $SOFTWARE_INSTALLER_TAG provisioned"
