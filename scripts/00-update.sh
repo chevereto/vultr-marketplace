2f37808cbe61b6dff55981850f0932fb8eb259aa
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2024-02-22 14:35:48 -0300

    grep vulr

--- scripts/00-update.sh
+++ scripts/00-update.sh
@@ -5,6 +5,7 @@ cd /tmp
 wget https://ewr1.vultrobjects.com/cloud_init_beta/cloud-init_universal_latest.deb
 apt-get update -y
 apt-get install -y /tmp/cloud-init_universal_latest.deb
+grep 'vultr' /etc/default/grub
 apt install -y apache2 libapache2-mod-php
 apt install -y mysql-server
 apt install -y php
