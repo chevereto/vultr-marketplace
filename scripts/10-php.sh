8bf6176d953d876a40069c1bf654cb641ff78e7e
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2024-02-22 14:35:58 -0300

    php 8.1

--- scripts/10-php.sh
+++ scripts/10-php.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat >/etc/php/8.2/apache2/conf.d/chevereto.ini <<EOM
+cat >/etc/php/8.1/apache2/conf.d/chevereto.ini <<EOM
 log_errors = On
 upload_max_filesize = 64M
 post_max_size = 64M
